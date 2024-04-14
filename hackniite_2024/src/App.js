d052577b54c6486aad241decb75363b3031182a6
dhalmy <91496056+dhalmy@users.noreply.github.com>
2024-04-14 18:04:37 -0500

    added search bar

--- hackniite_2024/src/App.js
+++ hackniite_2024/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import get_divvy_stations from './station_info'
 import DataPreprocessing from './DataPreprocessing';
 import DivvyMap from './DivvyMap';
+import SearchBar from './SearchBar';
 
 const App = () => {
   const divvy_stations = get_divvy_stations();
@@ -9,6 +10,7 @@ const App = () => {
 
     return (
     <div>
+      <SearchBar/>
       <DivvyMap
         stations={divvy_stations}
       />
