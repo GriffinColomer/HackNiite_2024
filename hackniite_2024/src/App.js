0bc84010d29c7b38831c738cff0cdb7bd8be6ebd
Griffin <griffincolomer@gmail.com>
2024-04-14 16:00:41 -0500

    Added in list of all divvy stops

--- hackniite_2024/src/App.js
+++ hackniite_2024/src/App.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+function divvy_stop(latitude, longitude, station_name){
+  this.latitude = latitude;
+  this.longitude = longitude;
+  this.station_name = station_name;
+  return this;
+};
+const all_divvy_stops = [];
+
+function parse_divvy_stops(data) {
+  for (let i = 0; i < data["data"]["stations"].length; i++) {
+    all_divvy_stops[i] = new divvy_stop(data["data"]["stations"][i]["lat"], data["data"]["stations"][i]["lon"], data["data"]["stations"][i]["name"]);
+}
+}
 
 const libraries = ['places'];
 const mapContainerStyle = {
@@ -8,10 +22,25 @@ const mapContainerStyle = {
 };
 const center = {
   lat: 7.2905715, // default latitude
-  lng: 80.6337262, // default longitude
+  lng: 80.6337262 // default longitude
 };
 
 const App = () => {
+  function fetch_data() {
+    fetch('https://gbfs.lyft.com/gbfs/2.3/chi/en/station_information.json')
+        .then(response => response.json())
+        .then(data => {
+          parse_divvy_stops(data)
+        });
+    console.log(all_divvy_stops)
+  }
+
+  useEffect(() => {
+    (async() => {
+      fetch_data();
+    })();
+  }, []);
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyBF8kUGA8g9S0RsMe6BJkgZb4GS4t6b0aE',
     libraries,
