3dde5b963ca9206aeee57a781aae121f87f680f7
Billy Bafia <wbafia@hawk.iit.edu>
2024-04-14 17:00:19 -0500

    Moving map to separate file.

--- hackniite_2024/src/App.js
+++ hackniite_2024/src/App.js
@@ -1,56 +1,18 @@
 import React from 'react';
 import get_divvy_stations from './station_info'
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import DataPreprocessing from './DataPreprocessing';
-
-const libraries = ['places'];
-const mapContainerStyle = {
-  width: '100%',
-  height: '100vh',
-};
-const loadPosition = {
-  lat: 41.8781,
-  lng: -87.6298
-};
+import DivvyMap from './DivvyMap';
 
 const App = () => {
-  get_divvy_stations()
+  get_divvy_stations();
   // console.log(divvy_stations)
 
-  const { isLoaded, loadError } = useLoadScript({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyBF8kUGA8g9S0RsMe6BJkgZb4GS4t6b0aE',
-    libraries,
-  });
-
-  const renderMap = () => {
-    // const onLoad = React.useCallback(
-    //   function onLoad(mapInstance) {
-    //     // do something with map Instance
-    //   }
-    // )
-
-    return <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={11}
-        center={loadPosition}
-      >
-        <Marker
-          position={loadPosition}
-          title='Chicago'
-        />
-      </GoogleMap>
-
+    return (
+    <div>
+      <DivvyMap />
       <DataPreprocessing />
     </div>
-  }
-
-  if (loadError) {
-    return <div>Error loading maps.</div>;
-  }
-
-  return isLoaded ? renderMap() : <div></div>;
+    );
 };
 
 export default App;
