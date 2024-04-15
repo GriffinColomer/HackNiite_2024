74e2a454c5259605093bf0f989a6a3fe8a9add5b
Billy Bafia <wbafia@hawk.iit.edu>
2024-04-14 22:50:39 -0500

    Adding tensor model.

--- tensorflow_model/build_model.py
+++ tensorflow_model/build_model.py
@@ -10,10 +10,9 @@ import tensorflowjs as tfjs
 data = pd.read_csv('..\hackniite_2024\src\data\DivvyBikeHistory-Subset-Michigan-Ida-Friday.csv')
 
 # Preprocess the data
-X = data['time']
+X = pd.to_datetime(data.pop('time'), format='%H:%M:%S')
+Y = data['diff']
 
-# Convert times
-Y = pd.to_datetime(data.pop('time'), format='%H:%M:%S')
 
 # Scale the data
 X_scales = [max(X), min(X)]
@@ -22,28 +21,28 @@ Y_scales = [max(Y), min(Y)]
 Y_data = (Y-Y_scales[1])/(Y_scales[0]-Y_scales[1])
 
 
-# regressor = Sequential()
-# regressor.add(Dense(units = 10, input_shape=(1,), activation="relu"))
-# regressor.add(Dense(units = 50, activation="relu"))
-# regressor.add(Dense(units = 500, activation="relu"))
-# regressor.add(Dense(units = 50, activation="relu"))
-# regressor.add(Dense(units = 10, activation="relu"))
-# regressor.add(Dense(units = 1))
-# regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-# regressor.summary()
+regressor = Sequential()
+regressor.add(Dense(units = 10, input_shape=(1,), activation="relu"))
+regressor.add(Dense(units = 50, activation="relu"))
+regressor.add(Dense(units = 500, activation="relu"))
+regressor.add(Dense(units = 50, activation="relu"))
+regressor.add(Dense(units = 10, activation="relu"))
+regressor.add(Dense(units = 1))
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+regressor.summary()
 
-# history = regressor.fit(X_data, Y_data, epochs = 30, validation_split=0.1)
-# plt.title('Model Loss')
-# plt.ylabel('Loss')
-# plt.xlabel('Epochs')
-# plt.plot(history.history['loss'])
-# plt.legend(['Train'])
-# plt.show()
+history = regressor.fit(X_data, Y_data, epochs = 30, validation_split=0.1)
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.plot(history.history['loss'])
+plt.legend(['Train'])
+plt.show()
 
-# predicted_y_data = regressor.predict(X_data)
-# predicted_y = predicted_y_data*(Y_scales[0]-Y_scales[1]) + Y_scales[1]
-# plt.scatter(X,Y)
-# plt.plot(X,predicted_y.reshape(-1,1))
+predicted_y_data = regressor.predict(X_data)
+predicted_y = predicted_y_data*(Y_scales[0]-Y_scales[1]) + Y_scales[1]
+plt.scatter(X,Y)
+plt.plot(X,predicted_y.reshape(-1,1))
 
-# tfjs.converters.save_keras_model(regressor, "model")
-# print(X_scales, Y_scales)
+tfjs.converters.save_keras_model(regressor, "model")
+print(X_scales, Y_scales)
