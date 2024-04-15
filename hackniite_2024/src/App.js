c399f4b5e343528df2d1dfdc9167eb13365289cb
dhalmy <91496056+dhalmy@users.noreply.github.com>
2024-04-14 21:02:03 -0500

    added some style

--- hackniite_2024/src/App.js
+++ hackniite_2024/src/App.js
@@ -3,13 +3,13 @@ import DivvyMap from './DivvyMap';
 import SearchBar from './SearchBar';
 import RouteMap from './RouteMap';
 import getDivvyStations from './helpers/StationInfo';
+import './App.css'; // Import the CSS file for styling
 
 const App = () => {
   const [divvyStations, setData] = useState([]);
   const [startStation, setStartStation] = useState('');
   const [endStation, setEndStation] = useState('');
   const [showRoute, setShowRoute] = useState(false);
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,31 +31,40 @@ const App = () => {
     setStartStation('');
     setEndStation('');
     setShowRoute(false);
-    window.location.reload()
+    window.location.reload();
   };
 
   return (
-    <div>
-      <SearchBar
-        stations={divvyStations}
-        onStartStationChange={setStartStation}
-        onEndStationChange={setEndStation}
-        startStation={startStation}
-        endStation={endStation}
-      />
-      <div>
-        <button onClick={handleShowRouteClick}>Show Route</button>
-        <button onClick={handleResetClick}>Reset</button>
-      </div>
-      {showRoute ? (
-        <RouteMap
+    <div className="app">
+      <h1 className="app-title">Divvy Station Route Planner</h1>
+      <div className="search-container">
+        <SearchBar
+          stations={divvyStations}
+          onStartStationChange={setStartStation}
+          onEndStationChange={setEndStation}
           startStation={startStation}
           endStation={endStation}
-          stations={divvyStations}
         />
-      ) : (
-        <DivvyMap stations={divvyStations} />
-      )}
+        <div className="button-container">
+          <button className="button" onClick={handleShowRouteClick}>
+            Show Route
+          </button>
+          <button className="button" onClick={handleResetClick}>
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="map-container">
+        {showRoute ? (
+          <RouteMap
+            startStation={startStation}
+            endStation={endStation}
+            stations={divvyStations}
+          />
+        ) : (
+          <DivvyMap stations={divvyStations} />
+        )}
+      </div>
     </div>
   );
 };
